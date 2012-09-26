971246013e043d5f84fa6746ad38ff4c25cda9b9
Ryan Richwine <ryan@redtailtechnology.com>
2012-09-26 16:56:28 -0700

    Add test for email lowercas saving

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -38,6 +38,16 @@ describe User do
 		it { should_not be_valid }
 	end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+
 	describe "when name is too long" do
 		before { @user.name = "a" * 51 }
 		it { should_not be_valid }
