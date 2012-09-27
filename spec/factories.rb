6c7ed74b760a3da4895b51aa3ca9429202d64d04
Ryan Richwine <ryan@redtailtechnology.com>
2012-09-27 12:07:23 -0700

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Ryan Richwine"
+    email    "ryan@redtailtechnology.com"
+    password "f00tball"
+    password_confirmation "f00tball"
+  end
+end
