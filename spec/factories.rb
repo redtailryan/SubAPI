033ff3230cb7b7371d5524ca316806e35fb932e0
Ryan Richwine <ryan@redtailtechnology.com>
2012-10-01 17:13:30 -0700

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Ryan Richwine"
-    email    "ryan@redtailtechnology.com"
-    password "f00tball"
-    password_confirmation "f00tball"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}   
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
